d84a5a52ed4960cbde9c7f7e268a2f8447fb8ef4
smoonmare <smoonmare@gmail.com>
2022-01-19 18:53:39 -0800

    Created Task-View component and added to the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskViewComponent } from './components/task-view/task-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
